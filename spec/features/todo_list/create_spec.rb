afe245deb8edda8f262053e0320274434af990f9
Ashari Juang <asharijuang@me.com>
2014-11-18 10:36:37 +0700

    add validation

---
+++ spec/features/todo_list/create_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "Membuat todo list"  do
+	it "redirects ke halaman index todo list dan success" do
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "My todo list"
+		fill_in "Description", with: "Ini adalah list untuk hari ini"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("My todo list")  
+	end
+
+	it "tampilkan error ketika halaman todo list tidak punya judul" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: ""
+		fill_in "Description", with: "Ini adalah list untuk hari ini"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Ini adalah list untuk hari ini") 
+
+	end
+
+	it "tampilkan error ketika halaman todo list punya judul kurang dari 3 karakter" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Hi"
+		fill_in "Description", with: "Ini adalah list untuk hari ini"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Ini adalah list untuk hari ini") 
+
+	end
+ end
