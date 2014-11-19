b5776eb3516b7ceb1531eaee40efbc0cd52bbc0d
Ashari Juang <asharijuang@me.com>
2014-11-19 08:26:34 +0700

    add validation description

--- spec/features/todo_list/create_spec.rb
+++ spec/features/todo_list/create_spec.rb
@@ -58,7 +58,7 @@ describe "Membuat todo list"  do
 		click_link "New Todo list"
 		expect(page).to have_content("New todo_list")
 
-		fill_in "Title", with: "Hi"
+		fill_in "Title", with: "Ashari Juang"
 		fill_in "Description", with: ""
 		click_button "Create Todo list"
 
@@ -66,8 +66,27 @@ describe "Membuat todo list"  do
 		expect(TodoList.count).to eq(0)
 
 		visit "/todo_lists"
-		expect(page).to_not have_content("Ini adalah list untuk hari ini") 
+		expect(page).to_not have_content("Ashari Juang") 
 
 	end
-	
+
+	it "tampilkan error ketika halaman todo list description kurang dari 5 karakter" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Ashari Juang"
+		fill_in "Description", with: "Food"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Ashari Juang") 
+
+	end
+
  end
