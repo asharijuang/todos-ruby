0c3edd1bde4489840a92853f76b483d54dffdb20
Ashari Juang <asharijuang@me.com>
2014-11-19 21:07:46 +0700

    Test editing todo list

--- spec/features/todo_list/edit_spec.rb
+++ spec/features/todo_list/edit_spec.rb
@@ -1,16 +1,57 @@
 require 'spec_helper'
 
 describe "Editing todo list"  do
-	it "Update todo list successfully with correct information" do
-		todo_list = TodoList.create(title: "Ashari Juang", description: "Ashari list.")
+	
+let!(:todo_list) { TodoList.create(title: "Ashari Juang", description: "Ashari list.")}
+	def update_todo_list(options={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "My description"
+
+		todo_list = options[:todo_list]
 
 		visit "/todo_lists"
-		within "todo_list_#{todo_list.id}" do
+		within "#todo_list_#{todo_list.id}" do
 			click_link "Edit"
 		end
 
-		fill_in "Title", with: "New title"
-		fill_in
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
+		click_button "Update Todo list"
 	end
 
+	it "Update todo list successfully with correct information" do
+		
+		update_todo_list 	todo_list: todo_list, 
+							title: "New title", 
+							description: "New description"
+
+		todo_list.reload
+
+		expect(page).to have_content("Todo list was successfully update")
+		expect(todo_list.title).to eq("New title")
+		expect(todo_list.description).to eq("New description")
+	end
+
+	it "display an error with too short  a title " do
+		update_todo_list todo_list: todo_list, title: ""
+		title = todo_list.title
+		todo_list.reload
+		expect(todo_list.title).to eq(title)
+		expect(page).to have_content("error")
+	end
+
+	it "display an error with too short  a title " do
+		update_todo_list todo_list: todo_list, title: "Hi"
+		expect(page).to have_content("error")
+	end
+
+	it "display an error with too short  a description " do
+		update_todo_list todo_list: todo_list, description: ""
+		expect(page).to have_content("error")
+	end
+
+	it "display an error with too short  a description " do
+		update_todo_list todo_list: todo_list, description: "Food"
+		expect(page).to have_content("error")
+	end
 end
