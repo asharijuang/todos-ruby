a312424ab08699bda1395b0785ee33e95e74a818
Ashari Juang <asharijuang@me.com>
2014-11-19 08:51:26 +0700

    refactor test spec

--- spec/features/todo_list/create_spec.rb
+++ spec/features/todo_list/create_spec.rb
@@ -1,14 +1,21 @@
 require 'spec_helper'
 
 describe "Membuat todo list"  do
-	it "redirects ke halaman index todo list dan success" do
+	def create_todo_list(options={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "This is my todo list"
+
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New todo_list")
 
-		fill_in "Title", with: "My todo list"
-		fill_in "Description", with: "Ini adalah list untuk hari ini"
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
+	end
+
+	it "redirects ke halaman index todo list dan success" do
+		create_todo_list
 
 		expect(page).to have_content("My todo list")  
 	end
@@ -16,13 +23,7 @@ describe "Membuat todo list"  do
 	it "tampilkan error ketika halaman todo list tidak punya judul" do
 		expect(TodoList.count).to eq(0)
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-
-		fill_in "Title", with: ""
-		fill_in "Description", with: "Ini adalah list untuk hari ini"
-		click_button "Create Todo list"
+		create_todo_list title:""
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
@@ -35,13 +36,7 @@ describe "Membuat todo list"  do
 	it "tampilkan error ketika halaman todo list punya judul kurang dari 3 karakter" do
 		expect(TodoList.count).to eq(0)
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-
-		fill_in "Title", with: "Hi"
-		fill_in "Description", with: "Ini adalah list untuk hari ini"
-		click_button "Create Todo list"
+		create_todo_list title:"Hi"
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
@@ -54,13 +49,7 @@ describe "Membuat todo list"  do
 	it "tampilkan error ketika halaman todo list tidak punya description" do
 		expect(TodoList.count).to eq(0)
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-
-		fill_in "Title", with: "Ashari Juang"
-		fill_in "Description", with: ""
-		click_button "Create Todo list"
+		create_todo_list title:"Ashari Juang", description:""
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
@@ -73,13 +62,7 @@ describe "Membuat todo list"  do
 	it "tampilkan error ketika halaman todo list description kurang dari 5 karakter" do
 		expect(TodoList.count).to eq(0)
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-
-		fill_in "Title", with: "Ashari Juang"
-		fill_in "Description", with: "Food"
-		click_button "Create Todo list"
+		create_todo_list title:"Ashari juang", description:"Food"
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
